93f44a196569e87b3b213aa9715041b2bccac290
cs947939 <sufam5@outlook.com>
2025-04-26 11:47:35 -0500

    create paymnet model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class paydata(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    iscomplete = models.BooleanField(default=False)
+    stirpe_session_id = models.CharField(max_length=255, blank=True, null=True)
+    stripe_link_id = models.CharField(max_length=255, blank=True, null=True)
