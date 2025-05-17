dc4cccc663542f665dd68b8793100934f934f1b0
coding2583571 <97715381+coding2583571@users.noreply.github.com>
2025-05-17 12:20:39 -0500

    Adding menu model

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stripe_product_id = models.CharField(max_length=255, blank=True, null=True)
+    preparation_process = models.TextField(max_length=1000, blank=True)
+    farm_name = models.CharField(max_length=255, blank=True)
+    farm_address = models.CharField(max_length=255, blank=True)
+
+
+    
