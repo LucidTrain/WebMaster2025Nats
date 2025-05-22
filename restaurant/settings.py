b57be26fa4c077df9fa4f8e3f458db00ea7201bf
coding323 <184160281+coding323@users.noreply.github.com>
2025-05-22 14:22:36 -0700

    f

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -60,9 +60,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'logistic.middleware.VisitMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-ROOT_URLCONF = 'restaurant.urls'
+ROOT_URLCONF = 'restaurant.urls' 
 
 TEMPLATES = [
     {
@@ -127,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
